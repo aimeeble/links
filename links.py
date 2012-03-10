c8fe54129e16694efd02cb99ede8942b08e96f85
Aimee <aimee@aimeeble.com>
2012-03-10 11:38:05 -0800

    clean up code a bit.  OO a bit.

--- links.py
+++ links.py
@@ -3,104 +3,142 @@ import werkzeug
 import os
 import errno
 
-BASE_URL="http://a/"
+BASE_URL="http://phoenix:5000/"
 CHARS = "123456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ"
 LEN = len(CHARS)
 
 REDIR = 1
 IMG = 2
 
+class ShortURL(object):
+   def __init__(self, short_id, short_url, long_url):
+      self.short_id = short_id
+      self.short_url = short_url
+      self.long_url = long_url
+
+      self.link_type = REDIR
+      self.mime_type = None
+
+      self.count = 0
+      self.refers = {}
+      self.ips = {}
+
+   def is_redir(self):
+      return self.link_type == REDIR
+
+   def is_img(self):
+      return self.link_type == IMG
+
+   def get_mime_type(self):
+      return self.mime_type
+
+   def get_short(self):
+      return self.short_url
+
+   def get_long(self):
+      return self.long_url
+
+   def follow_short_url(self, remote_ip, referer):
+      """Updates the stats after someone followed this URL.
+      """
+      self.count += 1
+
+      if referer not in self.refers:
+         self.refers[referer] = 1
+      else:
+         self.refers[referer] += 1
+
+      if remote_ip not in self.ips:
+         self.ips[remote_ip] = 1
+      else:
+         self.ips[remote_ip] += 1
+
+   def __str__(self):
+      return "(URL %s -> %s :: count:%s, IPs:%s, Refers:%s)" % \
+            (self.short_url, self.long_url, self.count, self.ips, self.refers)
+
+ShortInvalidException = Exception
+
+class ShortDB(object):
+   def __init__(self, prefix, db_url):
+      self.prefix = prefix
+      self.db_url = db_url
+      self.next_id = 0
+
+      self.db = {}
+
+   @classmethod
+   def encode(cls, my_id):
+      """Converts my_id into a code for use in the short URL.
+      """
+      res = ""
+      while True:
+         mod = my_id % LEN
+         c = CHARS[mod]
+         res = c + res
+
+         my_id = my_id / LEN
+         if my_id == 0:
+            break
+      return res
+
+   @classmethod
+   def decode(cls, short_id):
+      """Converts the short_id into a numeric ID.
+      """
+      res = 0
+      for c in short_id:
+         num = CHARS.find(c)
+         res = res * LEN + num
+      return res
+
+   def get_new_id(self):
+      my_id = self.next_id
+      self.next_id += 1
+      return my_id
+
+   def _shorten(self, my_id, long_url):
+      """Returns a complete shortened URL.
+      """
+      return self.prefix + ShortDB.encode(my_id)
+
+   def save(self, short_url_obj):
+      """Takes the ShortURL object and saves it to the DB.
+      """
+      pass
+
+   def load(self, short_code):
+      """Loads the info for short_url and returns a ShortURL object.
+      """
+      print "Loading %s" % short_code
+      if short_code not in self.db.keys():
+         raise ShortInvalidException("invalid short URL")
+
+      surl = self.db[short_code]
+      return surl
+
+   def new(self, long_url):
+      """Creates and returns a new ShortURL representing the url.
+      """
+      my_id = self.get_new_id()
+      short_url = self._shorten(my_id, long_url)
+
+      surl = ShortURL(my_id, short_url, long_url)
+
+      print "Saving id %s = %s" % (my_id, str(surl))
+      self.db[my_id] = surl
+      return surl
+
+   def __str__(self):
+      res = ""
+      for surl in self.db.values():
+         res += str(surl) + "\n"
+      return res
+
+
 app = flask.Flask(__name__)
+sdb = ShortDB(BASE_URL, None)
 
-# Map,
-#  {
-#     "id": {
-#        "url": "full url",
-#        "stats": {
-#           "count": int,
-#        }
-#     }
-#  }
-db = {}
-
-# Next ID that's available
-next_id = 1
-
-##############################################################################
-# Utilities
-def codify(num):
-   """Converts the number into a code
-   """
-   res = ""
-   while True:
-      mod = num % LEN
-      c = CHARS[mod]
-      res = c + res
-
-      num = num / LEN
-      if num == 0:
-         break
-
-   return res
-
-def decodify(rid):
-   """Converts the rid into a number code
-   """
-   res = 0
-   for c in rid:
-      num = CHARS.find(c)
-      res = res * LEN + num
-   return res
-
-def shorten(full_url, t = REDIR, m = None):
-   """Shortens the full_url into a new short URL.
-   """
-   global next_id, db
-   my_id = next_id
-   next_id += 1
-
-   db[my_id] = new_entry(full_url, t, m)
-   return BASE_URL + codify(my_id)
-
-def new_entry(fullurl, t = REDIR, m = None):
-   return {
-      "url": fullurl,
-      "type": t,
-      "mime": m,
-      "stats": {
-         "count": 0,
-         "refer": {},
-         "ip": {},
-      },
-   }
-
-def get_entry(rid):
-   """Looks up the rid in the db and returns the object.
-   """
-   try:
-      num = decodify(rid)
-   except:
-      return None
-   if not num in db.keys():
-      return None
-   return db[num]
-
-def update_entry(entry, request):
-   s = entry["stats"]
-
-   s["count"] += 1
-
-   refer = "direct"
-   if "referer" in request.headers:
-      refer = request.headers["referer"]
-   if refer not in s["refer"]:
-      s["refer"][refer] = 0
-   s["refer"][refer] += 1
-
-   ip = request.remote_addr
-   if ip not in s["ip"]:
-      s["ip"][ip] = 0
-   s["ip"][ip] += 1
 
 ##############################################################################
 # Landing page and creation
@@ -141,46 +179,55 @@ def new():
          full_url = fullpath
          t = IMG
          m = f.mimetype
-      short_url = shorten(full_url, t, m);
+
+      surl = sdb.new(full_url)
+      surl.link_type = t
+      surl.mime_type = m
+
+      short_url = surl.get_short()
 
       if flask.request.headers["accept"].find("application/json") >= 0:
-         return '{"status": "ok", "short_url": "%s"}' % short_url
+         return '{"status": "ok", "short_url": "%s"}' % surl.get_short()
 
    return flask.render_template("index.html", short_url=short_url)
 
 ##############################################################################
 # URL mappings
-@app.route("/<rid>", methods = ["GET"])
-def get_resource_id(rid):
-   entry = get_entry(rid)
-   if not entry:
+@app.route("/<encoded_short_code>", methods = ["GET"])
+def get_resource_id(encoded_short_code):
+   try:
+      short_code = ShortDB.decode(encoded_short_code)
+      surl = sdb.load(short_code)
+   except ShortInvalidException, e:
       return flask.make_response("not found", 404)
 
-   update_entry(entry, flask.request)
+   remote = flask.request.remote_addr
+   refer = "direct"
+   if "referer" in flask.request.headers:
+      refer = flask.request.headers["referer"]
+   surl.follow_short_url(flask.request.remote_addr, refer)
 
-   if entry["type"] == REDIR:
-      return flask.make_response("Moved", 302, {"Location": entry["url"]})
-   elif entry["type"] == IMG:
-      return flask.send_file(entry["url"], mimetype=entry["mime"])
+   if surl.is_redir():
+      return flask.make_response("Moved", 302, {"Location": surl.get_long()})
+   elif surl.is_img():
+      return flask.send_file(surl.get_long(), mimetype=surl.get_mime_type())
    else:
       return flask.make_response("invalid type", 500)
 
-@app.route("/<rid>+", methods = ["GET"])
-def get_resource_id_stats(rid):
-   entry = get_entry(rid)
-   if not entry:
+@app.route("/<encoded_short_code>+", methods = ["GET"])
+def get_resource_id_stats(encoded_short_code):
+   try:
+      short_code = ShortDB.decode(encoded_short_code)
+      surl = sdb.load(short_code)
+   except ShortInvalidException, e:
       return flask.make_response("not found", 404)
 
-   return str(entry)
+   return str(surl)
 
 @app.route("/dump", methods = ["GET"])
 def dump():
-   res = "<pre>"
-   for key, val in db.items():
-      res += "%s ->\n   %s\n" % (codify(key), val)
-   res += "</pre>"
-   return res
+   return "<pre>%s</pre>" % str(sdb)
 
 if __name__ == '__main__':
-   db[0] = new_entry("http://www.google.com")
+   sdb.new("http://www.google.com")
    app.run(debug=True,host='0.0.0.0')
