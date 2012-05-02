c7bd7ad5e40387be7d9b728911e7cd695b13fab9
Aimee <aimee@aimeeble.com>
2012-05-01 20:35:12 -0700

    add util to get info for a URL

--- linklib/util.py
+++ linklib/util.py
@@ -1,6 +1,8 @@
 import uuid
 import os
 import errno
+import urllib2
+import bs4
 
 class Util(object):
    @classmethod
@@ -23,3 +25,41 @@ class Util(object):
 
       Util._create_paths(fullpath)
       return fullpath
+
+class HeadRequest(urllib2.Request):
+   def get_method(self):
+      return "HEAD"
+
+def get_page_info(url):
+   info = {}
+
+   # Start by getting head.  We only do fancy stuff when it's the right type.
+   res = urllib2.urlopen(HeadRequest(url))
+   if res.headers.gettype() != "text/html":
+      info["mimetype"] = res.headers.gettype()
+      length = res.headers.get("Content-length")
+      if length:
+         info["length"] = length
+      return info
+
+   # This makes the assumption that HTML files are small, so we fetch it all.
+   res = urllib2.urlopen(url)
+   html = res.read()
+   soup = bs4.BeautifulSoup(html, "lxml")
+
+   if soup.title:
+      info["title"] = soup.title.string
+
+   info["meta"] = {}
+   for meta in soup.find_all("meta"):
+      key = meta.get("name")
+      val = meta.get("content")
+      if key and val:
+         info["meta"][key] = val
+
+   info["mimetype"] = res.headers.gettype()
+   length = res.headers.get("Content-length")
+   if length:
+      info["length"] = length
+
+   return info
