8a641be8501e0c34c37820917fbb8b0aa395b032
Aimee Langmaid <aimee@aimeeble.com>
2012-07-14 15:16:48 -0700

    handle exceptions more cleanly in http response

--- linkapi/util.py
+++ linkapi/util.py
@@ -5,8 +5,17 @@ import functools
 def jsonify(f):
    @functools.wraps(f)
    def _wrap(*args, **kwargs):
-      res = f(*args, **kwargs)
-      return flask.Response("%s\n" % json.dumps(res),
-                            content_type="application/json")
+      try:
+         res = f(*args, **kwargs)
+         return flask.Response("%s\n" % json.dumps(res),
+                              content_type="application/json")
+      except Exception, e:
+         res = {
+               "err": "%s" % (str(e)),
+            }
+         res = flask.Response("%s\n" % json.dumps(res),
+                               content_type="application/json")
+         res.status_code = 500
+         return res
    return _wrap
 
