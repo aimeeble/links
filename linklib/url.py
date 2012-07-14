69d6c0f0b4399f714ebadca63160e68b55f43580
Aimee Langmaid <aimee@aimeeble.com>
2012-07-14 11:25:31 -0700

    save last save time for links

--- linklib/url.py
+++ linklib/url.py
@@ -1,3 +1,6 @@
+import time
+
+
 class ShortURL(object):
    REDIR = 1
    IMG = 2
@@ -8,6 +11,7 @@ class ShortURL(object):
       self.short_url = short_url
       self.long_url = long_url
       self.info = info
+      self.latest_short = time.time()
 
       self.link_type = ShortURL.REDIR
       self.mime_type = None
@@ -51,12 +55,14 @@ class ShortURL(object):
             "link_type": self.link_type,
             "mime_type": self.mime_type,
             "info": self.info,
+            "latest_short": time.time()
          }
 
    def deserialize(self, data):
       self.short_code = data.get("short_code")
       self.short_url = data.get("short_url")
       self.long_url = data.get("long_url")
-      self.info = data.get("info")
       self.link_type = data.get("link_type")
       self.mime_type = data.get("mime_type")
+      self.info = data.get("info")
+      self.latest_short = data.get("latest_short")
