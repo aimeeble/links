9908b54cd86d7ec8d5c99e24b4addd5e78fffbc0
Aimee Langmaid <aimee@aimeeble.com>
2012-08-31 17:54:32 -0700

    upgrade: create missing thumbs.

--- upgrade.py
+++ upgrade.py
@@ -7,14 +7,22 @@ import pymongo
 from linklib.util import UploadedFile
 
 
-class ConvertSaveFileFormat(object):
+class ConvertBase(object):
+    def __init__(self):
+        self.con = pymongo.Connection('localhost')
+        self.db = pymongo.database.Database(self.con, 'links')
+
+    def convert(self):
+        raise RuntimeError('must override convert')
+
+
+class ConvertSaveFileFormat(ConvertBase):
     '''Converts from the static/IP/GUID filename convention to the
     static/content_hash/file.dat with symlink convention.
 
     '''
     def __init__(self):
-        self.con = pymongo.Connection('localhost')
-        self.db = pymongo.database.Database(self.con, 'links')
+        super(ConvertSaveFileFormat, self).__init__()
         self.files_to_delete = []
 
     def convert(self):
@@ -44,6 +52,40 @@ class ConvertSaveFileFormat(object):
                 self.db.urls.update({"short_code": row["short_code"]}, row)
 
 
+class CreateThumbnails(ConvertBase):
+    '''Creates thumbnails for any files which are missing it.
+
+    '''
+    def __init__(self):
+        super(CreateThumbnails, self).__init__()
+
+    def convert(self):
+        cur = self.db.urls.find({'link_type': 2})
+        for row in cur:
+
+            if not os.path.exists(row['long_url']):
+                continue
+
+            filename = os.path.basename(row["info"]["title"])
+
+            print '%s: thumbnailing %s' % (row['short_code'], filename)
+
+            with open(row["long_url"], "r") as fh:
+                uf = UploadedFile(
+                        None,
+                        stream=fh,
+                        mimetype=row["mime_type"],
+                        filename=filename)
+                uf._update_filenames()
+
+                if os.path.exists(uf.thumb_filename):
+                    print '\tskipping existing thumb'
+                    continue
+                uf._create_thumbnail()
+
+
 if __name__ == '__main__':
     c = ConvertSaveFileFormat()
+    c.convert()
+    c = CreateThumbnails()
     c.convert()
